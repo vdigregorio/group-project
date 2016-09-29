67b8be23566ce7fda04dcaa807595182928da1f6
Valeria Di Gregorio <clear>
2016-09-29 19:32:31 -0400

    omg were done

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('this is after the dom loaded');
+// document.addEventListener('DOMContentLoaded', function() {
+//     console.log('this is after the dom loaded');
 
-    var $listForm = document.querySelector('.list-maker-form');
-    console.log(window.location.search.charAt(1))
+//     var $listForm = document.querySelector('.list-maker-form');
+//     console.log(window.location.search.charAt(1))
 
-});
+// });
 
 
-console.log('this is before the dom loaded');
+// console.log('this is before the dom loaded');
