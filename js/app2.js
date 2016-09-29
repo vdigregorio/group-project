0157f07374844dcb1da8ffe6a8cc008ea26f5f83
Valeria Di Gregorio <clear>
2016-09-29 16:34:17 -0400

    jquery stuff

--- js/app2.js
+++ js/app2.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+console.log('app2.js is connected!')
 // var searchUrl = window.location.search.substring(1);
 // var split = [];
 // split = searchUrl.split("&");
@@ -10,22 +10,10 @@
 //   var value = arr[1];
 //   obj[key] = value;
 // }
-=======
 
-var searchUrl = window.location.search.substring(1);
-var split = [];
-split = searchUrl.split("&");
-var obj = {};
+// }
 
-for (i = 0; i < split.length; i++) {
-  var arr = [];
-  arr = split[i].split("=");
-  var key = arr[0];
-  var value = arr[1];
-  obj[key] = value;
-}
 
->>>>>>> 48fe4f0e775bcc66dd1d12c41b798938beaee90e
 // var select = key;
 // if (obj[select] === value1) {
 //   $('.cart').appendTo('NEW')
@@ -34,9 +22,43 @@ for (i = 0; i < split.length; i++) {
 // } else if (obj[select] === value3) {
 //   $('.cart').appendTo('NO SELECTION')
 // }
+$(document).ready(function(){
+// function getUrlVariables(url){
+//   var variables = [] , hash;
+//   var hashes = url.slice(url.indexof('?')+1).split('&');
+
+// for (var i=0; i < hashes.length; i++){
+//   hash = hashes[i].split('=');
+//   variables.push(hash[0]);
+//   variables[hash[0]] = hash[1];
+// }
+
+// return variables;
+// }
+// $('#personal').append($(this.variables));
+console.log(this.vars);
+$(function() {
+    function getUrlVars() {
+        var vars = [], hash;
+        console.log(window.query)
+        var hashes = window.query.slice(window.query.indexOf('?') + 1).split('&');
+        for(var i = 0; i < hashes.length; i++) {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[i];
+        }
+        return vars;
+    }
+    $('.btn2').click(function() {
+        var value = $(this).attr("value");
+        var test = getUrlVars(value)["name"];
+        alert(test);
+    });
+
+    getUrlVars()
+});
+
+})
 
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 48fe4f0e775bcc66dd1d12c41b798938beaee90e
